e798e307b479536435d3fc4293db7eb8a88536bb
Dan Sutherland <sutherl99@gmail.com>
2023-10-28 12:54:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
 
 streamlit.write('The user entered ', fruit_choice)
 
-streamlit.stop()
+#streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
