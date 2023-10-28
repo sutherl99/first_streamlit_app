3c5c9b7673a20b87c761370f76808dcb67e70f06
Dan Sutherland <sutherl99@gmail.com>
2023-10-28 12:04:26 -0600

    Update streamlit_app.py
    added new insert statement to test control flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,6 @@ streamlit.dataframe(my_data_rows)
 # New section to allow user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Tomato')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#This will not work correctly, but go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')
