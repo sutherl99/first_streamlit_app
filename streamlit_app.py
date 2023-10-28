59f8a7213f40835e739558c62dba0965111ee4a3
Dan Sutherland <sutherl99@gmail.com>
2023-10-28 13:00:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 #streamlit.stop()
 
-streamlit.header("View Our Fruit List - Add Your Favorites!:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
